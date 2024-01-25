cd55c593ecafb93be293d82667bb821a95a544fb
Sandra Flores <sfloresq07@gmail.com>
2024-01-25 18:15:41 -0300

    add function to show characters

--- main.js
+++ main.js
@@ -4,9 +4,50 @@ const publicKey = "&apikey=8dc1b4319121c44d910512cac0b27563";
 const hash = "&hash=d05cdf669e3cc07e2dbef29e98d4ed2f";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    
-    // Función para obtener datos de la API
-    const fetchData = async (title) => {
+    const filterSelect = document.getElementById("filter-select");
+    const searchButton = document.getElementById("search-button");
+
+    const buildComicCards = (data) => {
+        const cardContainer = document.getElementById("results-section");
+        cardContainer.innerHTML = "";
+
+        data.forEach((comic) => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            const thumbnailSrc = comic.thumbnail
+                ? `${comic.thumbnail.path}.${comic.thumbnail.extension}`
+                : 'placeholder.jpg';
+
+            card.innerHTML = `
+                <img src="${thumbnailSrc}" alt="${comic.title}">
+                <h2>${comic.title}</h2>
+            `;
+
+            cardContainer.appendChild(card);
+        });
+    };
+
+    const buildCharacterCards = (data) => {
+        const cardContainer = document.getElementById("results-section");
+        cardContainer.innerHTML = "";
+
+        data.forEach((character) => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            const thumbnailSrc = character.thumbnail
+                ? `${character.thumbnail.path}.${character.thumbnail.extension}`
+                : 'placeholder.jpg';
+
+            card.innerHTML = `
+                <img src="${thumbnailSrc}" alt="${character.name}">
+                <h2>${character.name}</h2>
+            `;
+
+            cardContainer.appendChild(card);
+        });
+    };
+
+    const fetchComics = async (title) => {
         try {
             let bringTitle = title ? `&title=${title}` : "";
             const apiUrl = `${urlBase}comics?${ts}${publicKey}${hash}${bringTitle}`;
@@ -19,29 +60,33 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
     };
 
-    // Función para construir las tarjetas
-    const buildCards = async () => {
-        const cardContainer = document.getElementById("results-section");
-        const data = await fetchData();
-
-        if (data) {
-            data.forEach((comic) => {
-                const card = document.createElement("div");
-                card.classList.add("card");
-                const thumbnailSrc = comic.thumbnail
-                    ? `${comic.thumbnail.path}.${comic.thumbnail.extension}`
-                    : 'placeholder.jpg';
-
-                card.innerHTML = `
-              <img src="${thumbnailSrc}" alt="${comic.title}">
-              <h2>${comic.title}</h2>
-            `;
-
-                cardContainer.appendChild(card);
-            });
+    const fetchCharacters = async (name) => {
+        try {
+            let bringName = name ? `&name=${name}` : "";
+            const apiUrl = `${urlBase}characters?${ts}${publicKey}${hash}${bringName}`;
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            console.log(data.data.results);
+            return data.data.results;
+        } catch (error) {
+            console.error("Error al obtener datos de la API:", error);
         }
     };
 
-  
-    buildCards();
+    const handleSearch = async () => {
+        const selectedValue = filterSelect.value;
+
+        if (selectedValue === "title") {
+            const comicsData = await fetchComics();
+            buildComicCards(comicsData);
+        } else if (selectedValue === "name") {
+            const charactersData = await fetchCharacters();
+            buildCharacterCards(charactersData);
+        }
+    };
+
+    // Construir tarjetas de cómics por defecto
+    await handleSearch();
+
+    searchButton.addEventListener("click", handleSearch);
 });
